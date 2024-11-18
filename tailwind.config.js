1926ba0e82689ec25cbed8a4dd2b0c2361aa1f7c
Atchayavijay <atchuvijay83@gmail.com>
2024-11-18 23:19:33 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.html'],
+  theme: {
+    extend: {
+      color: {
+        primary: "#361BA8",
+        secondary: "#787AC0",
+      },
+    },
+    plugins: [],
+  }
+
+}
